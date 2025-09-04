3afbd3bee700edc89162ee5084c450a30932620e
roman-ks <roman-ks@users.noreply.github.com>
2025-09-04 21:14:56 +0300

    uncomment png decoder

--- src/main/esp32/decoder/png_decoder.cpp
+++ src/main/esp32/decoder/png_decoder.cpp
@@ -1,67 +1,71 @@
-// #include "png_decoder.h"
+#include "png_decoder.h"
+#include "../../core/configs.h"
+#include <LittleFS.h>
 
-// PNGDecoder::PNGDecoder() {
-// }
-// PNGDecoder::~PNGDecoder() {
-//     if(file){
-//         file.close();
-//     }
-// }
-// void PNGDecoder::init() {
-//     // Initialize PNG decoder if needed
-// }
+PNGDecoder::PNGDecoder() {
+}
+PNGDecoder::~PNGDecoder() {
+  s_drawCallback = nullptr;
+}
+void PNGDecoder::init() {
+    // Initialize PNG decoder if needed
+}
 
-// void PNGDecoder::decode(char* filepath, std::function<int(void*)> drawCallback, void* holder) {
+void PNGDecoder::decode(char* filepath, std::function<int(void*)> drawCallback, void* holder) {
 
-//     PNG &png = *static_cast<PNG*>(holder);
-//     this->drawCallback = drawCallback;
-//     Serial.println("Opening image..");
-//     int16_t rc = png.open(strname.c_str(), pngOpen, pngClose, pngRead, pngSeek, pngDraw);
-//     if (rc == PNG_SUCCESS) {
-//         Serial.printf("image specs: (%d x %d), %d bpp, pixel type: %d\n", png.getWidth(), png.getHeight(), png.getBpp(), png.getPixelType());
-//         uint32_t dt = millis();
-//         if (png.getWidth() > MAX_IMAGE_WIDTH) {
-//           Serial.println("Image too wide for allocated line buffer size!");
-//         }
-//         else {
-//           rc = png.decode(NULL, 0);
-//           png.close();
-//         }
-//         // How long did rendering take...
-//         Serial.print(millis()-dt); Serial.println("ms");
-//     }
-//     this->drawCallback = nullptr;
-// }
+    PNG &png = *static_cast<PNG*>(holder);
+    s_drawCallback = drawCallback;
+    Serial.println("Opening image..");
+    int16_t rc = png.open(filepath, &PNGDecoder::pngOpen, &PNGDecoder::pngClose, 
+        &PNGDecoder::pngRead, &PNGDecoder::pngSeek, &PNGDecoder::pngDraw);
+    if (rc == PNG_SUCCESS) {
+        Serial.printf("image specs: (%d x %d), %d bpp, pixel type: %d\n", png.getWidth(), png.getHeight(), png.getBpp(), png.getPixelType());
+        uint32_t dt = millis();
+        if (png.getWidth() > MAX_IMAGE_WIDTH) {
+          Serial.println("Image too wide for allocated line buffer size!");
+        }
+        else {
+          rc = png.decode(NULL, 0);
+          png.close();
+        }
+        // How long did rendering take...
+        Serial.print(millis()-dt); Serial.println("ms");
+    }
+    s_drawCallback = nullptr;
+}
 
-// void * PNGDecoder::pngOpen(const char *filename, int32_t *size) {
-//   Serial.printf("Attempting to open %s\n", filename);
-//   pngfile = FileSys.open(filename, "r");
-//   *size = pngfile.size();
-//   return &pngfile;
-// }
+void * PNGDecoder::pngOpen(const char *filename, int32_t *size) {
+  Serial.printf("Attempting to open %s\n", filename);
+  File file = LittleFS.open(filename, "r");
+  *size = file.size();
+  return &file;
+}
 
-// void PNGDecoder::pngClose(void *handle) {
-//   File pngfile = *((File*)handle);
-//   if (pngfile) pngfile.close();
-// }
+void PNGDecoder::pngClose(void *handle) {
+  File *file = static_cast<File*>(handle);
+  if (file) file->close();
+}
 
-// int32_t PNGDEcoder::pngRead(PNGFILE *page, uint8_t *buffer, int32_t length) {
-//   if (!pngfile) return 0;
-//   page = page; // Avoid warning
-//   return pngfile.read(buffer, length);
-// }
+int32_t PNGDecoder::pngRead(PNGFILE *page, uint8_t *buffer, int32_t length) {
+  File *file = static_cast<File*>(page->fHandle);
+  if (!file) return 0;
+  page = page; // Avoid warning
+  return file->read(buffer, length);
+}
 
-// int32_t PNGDecoder::pngSeek(PNGFILE *page, int32_t position) {
-//   if (!pngfile) return 0;
-//   page = page; // Avoid warning
-//   return pngfile.seek(position);
-// }
+int32_t PNGDecoder::pngSeek(PNGFILE *page, int32_t position) {
+  File *file = static_cast<File*>(page->fHandle);
+  if (!file) return 0;
+  page = page; // Avoid warning
+  return file->seek(position);
+}
 
-// int32_t PNGDecoder::pngDraw(PNGDRAW *pDraw) {
-//   if (!pngfile) return 0;
-//   if (drawCallback) {
-//       return drawCallback((void*)pDraw);
-//   }
-//   return 0;
-// }
+// wrapper to have function delegeate to std::function and erase param type
+int PNGDecoder::pngDraw(PNGDRAW *pDraw) {
+//   if (!file) return 0;
+  if (s_drawCallback) {
+      return s_drawCallback((void*)pDraw);
+  }
+  return 0;
+}
 
--- src/main/esp32/decoder/png_decoder.h
+++ src/main/esp32/decoder/png_decoder.h
@@ -1,27 +1,26 @@
-// #ifndef __ESP_GALLERY_PNG_DECODER_H__
-// #define __ESP_GALLERY_PNG_DECODER_H__
+#ifndef __ESP_GALLERY_PNG_DECODER_H__
+#define __ESP_GALLERY_PNG_DECODER_H__
 
-// #include "decoder.h"
-// #include <PNGdec.h>
-// #include <LittleFS.h>
+#include "decoder.h"
+#include <PNGdec.h>
+#include <FS.h>
 
 
-// class PNGDecoder : public Decoder {
-//     public:
-//         PNGDecoder();
-//         ~PNGDecoder();
+class PNGDecoder : public Decoder {
+    public:
+        PNGDecoder();
+        ~PNGDecoder();
 
-//         void init() override;
-//         void decode(char* filepath, std::function<int(void*)> drawCallback, void* returned) override;
-//     private:
-//         void * pngOpen(const char *filename, int32_t *size);
-//         void pngClose(void *handle);
-//         int32_t pngRead(PNGFILE *page, uint8_t *buffer, int32_t length);
-//         int32_t pngSeek(PNGFILE *page, int32_t position);
-//         int32_t pngDraw(PNGDRAW *pDraw);
-//         PNG getPNG();
+        void init() override;
+        void decode(char* filepath, std::function<int(void*)> drawCallback, void* returned) override;
+    private:
+        static void * pngOpen(const char *filename, int32_t *size);
+        static void pngClose(void *handle);
+        static int32_t pngRead(PNGFILE *page, uint8_t *buffer, int32_t length);
+        static int32_t pngSeek(PNGFILE *page, int32_t position);
+        static int pngDraw(PNGDRAW *pDraw);
+        PNG getPNG();
 
-//         File file;
-//         std::function<void(void*)> drawCallback;
-// };
-// #endif
+        static std::function<int(void*)> s_drawCallback;
+};
+#endif

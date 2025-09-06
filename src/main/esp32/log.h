787a5f1752ebc37aa880563c65b47d24dc8df7f6
roman-ks <roman-ks@users.noreply.github.com>
2025-09-06 12:05:36 +0300

    pad logger name

--- src/main/esp32/log.h
+++ src/main/esp32/log.h
@@ -4,7 +4,7 @@
 #include <cstring>
 
 inline const char* normalizePath(const char* path){
-    static char buf[256];
+    static char buf[512];
     size_t len = strlen(path) + 1;
     if(len >= sizeof(buf)) len = sizeof(buf)-1;
     for(size_t i=0; i<len; i++){
@@ -17,12 +17,19 @@ inline const char* normalizePath(const char* path){
 #define FILE_RELATIVE(file) \
     (strstr(normalizePath(file), "/src/") ? (strstr(normalizePath(file), "/src/") + 5) : file)
 
+inline const char* createLoggerName(char *file, long line){
+    static char buf[512];
+    sprintf(buf, "[%s:%d]", FILE_RELATIVE(file), line);
+    return buf;
+}
+
+
 #define LOGF(fmt, ...) \
     do { \
-        Serial.printf("[%s:%d] " fmt, FILE_RELATIVE(__FILE__), __LINE__, ##__VA_ARGS__); \
+        Serial.printf("%-40s " fmt, createLoggerName(__FILE__, __LINE__), ##__VA_ARGS__); \
     } while(0)
 
 #define LOG(msg) \
     do { \
-        Serial.printf("[%s:%d] " msg "\n", FILE_RELATIVE(__FILE__), __LINE__); \
+        Serial.printf("%-40s " msg "\n", createLoggerName(__FILE__, __LINE__)); \
     } while(0)

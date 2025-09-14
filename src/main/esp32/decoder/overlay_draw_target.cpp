f454e8653a0511833bc110e33a88655612a48ea6
roman-ks <roman-ks@users.noreply.github.com>
2025-09-14 17:35:20 +0300

    optimise overlay_draw_target

--- src/main/esp32/decoder/overlay_draw_target.cpp
+++ src/main/esp32/decoder/overlay_draw_target.cpp
@@ -38,36 +38,34 @@ void OverlayDrawTarget::setAddrWindow(uint16_t x, uint16_t y, uint16_t w, uint16
     delegate->setAddrWindow(x, y, w, h);
 }
 
-void OverlayDrawTarget::pushPixels(const void *pixels, uint32_t count) {
+void OverlayDrawTarget::pushPixels(const void *pixelsPtr, uint32_t count) {
+        const uint16_t* src = static_cast<const uint16_t*>(pixelsPtr);
 
-    const uint16_t* src = static_cast<const uint16_t*>(pixels);
+        // compute intersection rectangle
+        int ix0 = std::max<int>(m_windowX, m_overlayX);
+        int iy0 = std::max<int>(m_windowY, m_overlayY);
+        int ix1 = std::min<int>(m_windowX + m_windowW, m_overlayX + m_overlayWidth);
+        // m_windowH is offset, +1 to convert to count
+        int iy1 = std::min<int>(m_windowY + m_windowH + 1, m_overlayY + m_overlayHeight);
 
-    uint32_t i = 0;
-    for (uint16_t row = 0; row <= m_windowH; ++row) {
-        for (uint16_t col = 0; col < m_windowW; ++col) {
-            uint16_t absX = m_windowX + col;
-            uint16_t absY = m_windowY + row;
-
-            bool inOverlay =
-                absX >= m_overlayX &&
-                absX < (m_overlayX + m_overlayWidth) &&
-                absY >= m_overlayY &&
-                absY < (m_overlayY + m_overlayHeight);
-
-            if (inOverlay) {
-                LOGF("In overlay %d, %d", absX, absY);
-                uint16_t ox = absX - m_overlayX;
-                uint16_t oy = absY - m_overlayY;
-                m_outBuffer[i] = m_overlayPixels[oy * m_overlayWidth + ox];
-            } else {
-                m_outBuffer[i] = src[i];
-            }
-
-            ++i;
-            if (i >= count) break; // safety
+        if (ix0 >= ix1 || iy0 >= iy1) {
+            // no overlap, delegate directly
+            delegate->pushPixels(pixelsPtr, count);
+            return;
         }
-        if (i >= count) break;
-    }
 
-    delegate->pushPixels(m_outBuffer, count);
-}
+        // make a copy of the window pixels
+        for (uint32_t i = 0; i < count; ++i)
+            memcpy(m_outBuffer, src, count);
+
+        // overlay only on intersection area
+        for (int iy = iy0; iy < iy1; ++iy) {
+            for (int ix = ix0; ix < ix1; ++ix) {
+                int overlayIdx = (iy - m_overlayY) * m_overlayWidth + (ix - m_overlayX);
+                int bufferIdx  = (iy - m_windowY) * m_windowW + (ix - m_windowX);
+                m_outBuffer[bufferIdx] = m_overlayPixels[overlayIdx];
+            }
+        }
+
+        delegate->pushPixels(m_outBuffer, count);
+    }

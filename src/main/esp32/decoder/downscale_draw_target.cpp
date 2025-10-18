d76bf08bfa94698a45cc7458054c7517ab6413f9
roman-ks <roman-ks@users.noreply.github.com>
2025-10-18 10:52:19 +0300

    use array instead of vector in DownscaleDrawTarget

--- src/main/esp32/decoder/downscale_draw_target.cpp
+++ src/main/esp32/decoder/downscale_draw_target.cpp
@@ -1,5 +1,6 @@
 #include "downscale_draw_target.h"
 #include <cstring>
+#include "../../core/configs.h"
 
 DownscaleDrawTarget::DownscaleDrawTarget(IDrawTarget *delegate, 
                                          uint16_t widthScale,
@@ -8,8 +9,12 @@ DownscaleDrawTarget::DownscaleDrawTarget(IDrawTarget *delegate,
       m_widthScale(widthScale),
       m_heightScale(heightScale),
       m_windowX(0), m_windowY(0), m_windowW(0), m_windowH(0),
-      m_cursorX(0), m_cursorY(0)
+      m_cursorX(0), m_cursorY(0),
+      m_downBufCount(0)
+
 {
+    m_downBuf = std::make_unique<uint16_t[]>(MAX_IMAGE_WIDTH/ widthScale * 
+                                             MAX_IMAGE_HEIGHT/ heightScale);
 }
 
 void DownscaleDrawTarget::setAddrWindow(uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
@@ -26,9 +31,8 @@ void DownscaleDrawTarget::setAddrWindow(uint16_t x, uint16_t y, uint16_t w, uint
 
     delegate->setAddrWindow(x / m_widthScale, y / m_heightScale, dw, dh);
 
-    // Allocate buffer for one chunk of downscaled pixels
-    m_downBuf.clear();
-    m_downBuf.reserve(dw * dh);
+    // Reset fixed buffer
+    m_downBufCount = 0;
 }
 
 void DownscaleDrawTarget::pushPixels(const void *pixels, uint32_t count) {
@@ -40,7 +44,7 @@ void DownscaleDrawTarget::pushPixels(const void *pixels, uint32_t count) {
         uint16_t absY = m_windowY + m_cursorY;
 
         if ((absX % m_widthScale == 0) && (absY % m_heightScale == 0)) {
-            m_downBuf.push_back(src[i]);
+            m_downBuf[m_downBufCount++] = src[i];
         }
 
         // Move cursor
@@ -52,8 +56,8 @@ void DownscaleDrawTarget::pushPixels(const void *pixels, uint32_t count) {
     }
 
     // Flush if we collected enough downscaled pixels for a row
-    if (!m_downBuf.empty()) {
-        delegate->pushPixels(m_downBuf.data(), m_downBuf.size());
-        m_downBuf.clear();
+    if (m_downBufCount > 0) {
+        delegate->pushPixels(m_downBuf.get(), static_cast<uint32_t>(m_downBufCount));
+        m_downBufCount = 0;
     }
 }
